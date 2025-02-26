02f35343251f77fd0e3c3b99a46a252e2e01fe4e
Damon S. Green II <damonsgreen2@gmail.com>
2025-02-25 19:07:28 -0500

    basic generation completed
    need to better isolate generation and adjustment rules. Add breakable walls. test with multiple levels, etc.

--- Assets/Scripts/MazeGenerator/MazeCell/MazeCell.cs
+++ Assets/Scripts/MazeGenerator/MazeCell/MazeCell.cs
@@ -1,36 +1,167 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MazeCell : MonoBehaviour
-{ 
-    [SerializeField] GameObject LeftWall;       private bool hasLeftWall {  get; set; }
-    public bool DestroyLeftWall() { hasLeftWall = false; LeftWall.SetActive(false); return true; ;    }
-    
-    [SerializeField] GameObject RightWall;       public bool hasRightWall { get; private set ; }
-    public bool DestroyRightWall()    { hasRightWall = false; RightWall.SetActive(false);  return true; }
+{
+    [SerializeField] public GameObject LeftWall;
+    public bool hasLeftWall { get; set; }
+    public void DestroyLeftWall()
+    {
+        hasLeftWall = false; LeftWall.SetActive(false); numOfWalls -= 1;
+        remainingWalls.Remove(LeftWall);
+    }
 
-    [SerializeField] GameObject FrontWall;       public bool hasFrontWall { get; private set ; }
-    public bool DestroyFrontWall()    { hasFrontWall = false; FrontWall.SetActive(false);  return true; }
+    [SerializeField] public GameObject RightWall;
+    public bool hasRightWall { get; private set; }
+    public void DestroyRightWall()
+    {
+        hasRightWall = false; RightWall.SetActive(false); numOfWalls -= 1;
+        remainingWalls.Remove(RightWall);
+    }
 
-    [SerializeField] GameObject BackWall;       public bool hasBackWall { get; private set ; }
-    public bool DestroyBackWall()    { hasBackWall = false; BackWall.SetActive(false); return true;  }
+    [SerializeField] public GameObject FrontWall;
+    public bool hasFrontWall { get; private set; }
+    public void DestroyFrontWall()
+    {
+        hasFrontWall = false; FrontWall.SetActive(false); numOfWalls -= 1;
+        remainingWalls.Remove(FrontWall);
+    }
 
-    [SerializeField] GameObject Cieling;        public bool hasCieling { get; private set ; }
-    public bool DestroyCieling() { hasCieling = false; Cieling.SetActive(false); return true; }
+    [SerializeField] public GameObject BackWall;
+    public bool hasBackWall { get; private set; }
+    public void DestroyBackWall()
+    {
+        hasBackWall = false; BackWall.SetActive(false); numOfWalls -= 1;
+        remainingWalls.Remove(BackWall);
+    }
 
-    [SerializeField] GameObject Floor;         public bool hasFloor {  get; private set; }
-    public bool DestroyFloor()     { hasFloor = false; Floor.SetActive(false); return true;   }
+    [SerializeField] public GameObject Ceiling;
+    public bool hasCeiling { get; private set; }
+    public void DestroyCeiling()
+    {
+        hasCeiling = false; Ceiling.SetActive(false); numOfWalls -= 1;
+        remainingWalls.Remove(Ceiling);
+    }
+
+    [SerializeField] public GameObject Floor;
+    public bool hasFloor { get; private set; }
+    public void DestroyFloor()
+    {
+        hasFloor = false; Floor.SetActive(false); numOfWalls -= 1;
+        remainingWalls.Remove(Floor);
+    }
 
 
-    [SerializeField] GameObject UnvisitedCell;         public bool cellWasVisited {  get; private set; }
-    public bool Visit() { cellWasVisited = true; UnvisitedCell.SetActive(false);return true; } 
+    [SerializeField] public GameObject UnvisitedCell;
+    public bool cellWasVisited { get; private set; }
+    public void Visit()
+    {
+        cellWasVisited = true; UnvisitedCell.SetActive(false);
+    }
 
 
+    public int numOfWalls { get; private set; }
+    public bool isBoundaryCell = false;
+    public List<GameObject> boundaryWalls = new List<GameObject>();
+    public List<GameObject> remainingWalls = new List<GameObject>();
+
+    private void Awake()
+    {
+        cellWasVisited = false;
+        boundaryWalls.Clear();
+        remainingWalls.Clear();
+        numOfWalls = 0;
+
+        if (LeftWall != null)
+        {
+            hasLeftWall = true;
+            remainingWalls.Add(LeftWall);
+            numOfWalls += 1;
+        }
+
+        if (RightWall != null)
+        {
+            hasRightWall = true;
+            remainingWalls.Add(RightWall);
+            numOfWalls += 1;
+        }
+
+        if (FrontWall != null)
+        {
+            hasFrontWall = true;
+            remainingWalls.Add(FrontWall);
+            numOfWalls += 1;
+        }
+
+        if (BackWall != null)
+        {
+            hasBackWall = true;
+            remainingWalls.Add(BackWall);
+            numOfWalls += 1;
+        }
+
+        if (Ceiling != null)
+        {
+            hasCeiling = true;
+            remainingWalls.Add(Ceiling);
+            numOfWalls += 1;
+        }
+
+        if (Floor != null)
+        {
+            hasFloor = true;
+            remainingWalls.Add(Floor);
+            numOfWalls += 1;
+        }
 
 
+    }
 
+    public void DeleteRandomRemainingWall(int mazeHeight)
+    {
+        if (remainingWalls.Count > 0)
+        {
+            int randomIndex = Random.Range(0, remainingWalls.Count);
+            GameObject wall = remainingWalls[randomIndex];
+            if (!boundaryWalls.Contains(wall))
+            {
+                if (wall == LeftWall)
+                {
+                    DestroyLeftWall();
+                }
+                else if (wall == RightWall)
+                {
+                    DestroyRightWall();
+                }
+                else if (wall == FrontWall)
+                {
+                    DestroyFrontWall();
+                }
+                else if (wall == BackWall)
+                {
+                    DestroyBackWall();
+                }
+                else if (mazeHeight > 1)
+                {
+                    if (wall == Ceiling)
+                    {
+                        DestroyCeiling();
+                    }
+                    else if (wall == Floor)
+                    {
+                        DestroyFloor();
+                    }
+                }
+            }
+        }
 
+    }
 
-
-
-
+    public void RemoveBoundaryWallsFromRemainingWalls()
+    {
+        foreach (GameObject wall in boundaryWalls)
+        {
+            remainingWalls.Remove(wall);
+        }
+    }
 }
--- Assets/Scripts/MazeGenerator/MazeCell/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator/MazeCell/MazeGenerator.cs
@@ -10,58 +10,336 @@ public class MazeGenerator : MonoBehaviour
     [SerializeField] private int mazeWidth = 10;
     [SerializeField] private int mazeDepth = 10;
     [SerializeField] private int mazeHeight = 1;
-    [SerializeField] public int mazePrefabScale = 1;
+    [SerializeField] private float minXScale = 1;
+    [SerializeField] private float minYScale = 1;
+    [SerializeField] private float minZScale = 1;
+    [SerializeField] public Vector3 mazePrefabScale = new Vector3(1, 1, 1);
+    [SerializeField] Color boundaryCellColor;
+    [SerializeField][Range(0f, 0.45f)] float minWallPercent = 0.45f;
+    [SerializeField][Range(0f, 0.5f)] float maxIsolatedCells = 0.25f;
 
-    public MazeCell[,] mazeLevel;
-    public List<MazeCell[,]> levels = new List<MazeCell[,]>();
+    public bool changeBoundaryCellColor = false;
+    public bool enableTopDownView = false;
+    public int totalCells;
+    public List<MazeCell> boundaryCells = new List<MazeCell>();
+    public int boundaryCellsCount;
+    public int innerCells;
+    public int unvisitedCells;
+    private MazeCell prevCell;
+    private MazeCell currCell;
+    public bool generationInProgress = false;
+    public MazeCell[,,] mazeLevel;
+    public bool isFinishedRefining = false;
+    public bool isFinishedSolvingBlockages = false;
+    public bool isFinishedAddingBackWalls = false;
 
 
-    private IEnumerator Start()
+    private void Start()
     {
         Initialize();
-
-        yield return GenerateMaze(null, levels[0][0, 0]) ;
-
-
+        StartMazeGeneration();
+        if (enableTopDownView)
+        {
+            foreach (MazeCell cell in mazeLevel)
+            {
+                cell.DestroyCeiling();
+            }
+        }
     }
+
     public void Initialize()
     {
+        totalCells = (mazeWidth * mazeHeight * mazeDepth);
+        unvisitedCells = totalCells;
+        if (mazePrefabScale.x < minXScale || mazePrefabScale.y < minYScale || mazePrefabScale.z < minZScale)
+        {
+            if (mazePrefabScale.x < 1)
+            {
+                mazePrefabScale.x = minXScale;
+                Debug.LogError("Maze prefab X scale must be greater than 1");
+            }
+            if (mazePrefabScale.y < 0.5)
+            {
+                mazePrefabScale.y = minYScale;
+                Debug.LogError("Maze prefab Y scale must be greater than 0.5");
+            }
+            if (mazePrefabScale.z < 1)
+            {
+                mazePrefabScale.z = minZScale;
+                Debug.LogError("Maze prefab Z scale must be greater than 1");
+            }
+        }
+        mazeCell.transform.localScale = mazePrefabScale;
         cell = mazeCell.GetComponent<MazeCell>();
-        levels.Clear();
-
+        mazeLevel = new MazeCell[mazeWidth, mazeHeight, mazeDepth];
         for (int k = 0; k < mazeHeight; k++)
         {
-            mazeLevel = new MazeCell[mazeWidth, mazeDepth];
             for (int i = 0; i < mazeWidth; i++)
             {
                 for (int j = 0; j < mazeDepth; j++)
                 {
-                    mazeLevel[i, j] = Instantiate(cell, new Vector3(i, k, j), Quaternion.identity);
+                    Vector3 cellPosition = new Vector3(i * mazePrefabScale.x, k * mazePrefabScale.y, j * mazePrefabScale.z);
+                    mazeLevel[i, k, j] = Instantiate(cell, cellPosition, Quaternion.identity);
                 }
             }
-            levels.Add(mazeLevel);
-            transform.position = new Vector3(transform.position.x, (transform.position.y + 1), transform.position.z);
+            transform.position = new Vector3(transform.position.x, (transform.position.y + mazePrefabScale.y), transform.position.z);
+        }
+        MarkBoundaryCells();
+    }
+
+    public void MarkBoundaryCells()
+    {
+        for (int i = 0; i < mazeWidth; i++)
+        {
+            for (int j = 0; j < mazeDepth; j++)
+            {
+                for (int k = 0; k < mazeHeight; k++)
+                {
+                    if (IsBoundaryCell(mazeLevel[i, k, j]))
+                    {
+                        if (mazeLevel[i, k, j].isBoundaryCell)
+                        {
+                            boundaryCells.Add(mazeLevel[i, k, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public bool IsBoundaryCell(MazeCell cell)
+    {
+        int x = (int)(cell.transform.position.x);
+        int y = (int)(cell.transform.position.y);
+        int z = (int)(cell.transform.position.z);
+
+        bool isBoundary = false;
+
+        if (x == 0 || x == (mazeWidth - 1) * mazePrefabScale.x || y == 0 || y == (mazeHeight - 1) * mazePrefabScale.y || z == 0 || z == (mazeDepth - 1) * mazePrefabScale.z)
+        {
+            isBoundary = true;
+            MarkBoundaryWalls(cell, x, y, z);
+        }
+
+        return isBoundary;
+    }
+
+
+    public void MarkBoundaryWalls(MazeCell cell, int x, int y, int z)
+    {
+
+
+        // Mark boundaries based on the position of the cell, taking scaling into account
+        if (x <= 0)
+        {
+            cell.boundaryWalls.Add(cell.RightWall);
+            cell.RightWall.SetActive(true);
+            cell.RightWall.GetComponentInChildren<Renderer>().material.color = boundaryCellColor;
+        }
+        if (x >= (mazeWidth - 1) * mazePrefabScale.x)
+        {
+            cell.boundaryWalls.Add(cell.LeftWall);
+            cell.LeftWall.SetActive(true);
+            cell.LeftWall.GetComponentInChildren<Renderer>().material.color = boundaryCellColor;
+        }
+        if (z <= 0)
+        {
+            cell.boundaryWalls.Add(cell.BackWall);
+            cell.BackWall.SetActive(true);
+            cell.BackWall.GetComponentInChildren<Renderer>().material.color = boundaryCellColor;
+        }
+        if (z >= (mazeDepth - 1) * mazePrefabScale.z)
+        {
+            cell.boundaryWalls.Add(cell.FrontWall);
+            cell.FrontWall.SetActive(true);
+            cell.FrontWall.GetComponentInChildren<Renderer>().material.color = boundaryCellColor;
+        }
+
+        if (mazeHeight > 1)
+        {
+            if (y <= 0)
+            {
+                cell.boundaryWalls.Add(cell.Floor);
+                cell.Floor.SetActive(true);
+                cell.Floor.GetComponentInChildren<Renderer>().material.color = boundaryCellColor;
+            }
+            if (y >= (mazeHeight - 1) * mazePrefabScale.y)
+            {
+                cell.boundaryWalls.Add(cell.Ceiling);
+                cell.Ceiling.SetActive(true);
+                cell.Ceiling.GetComponentInChildren<Renderer>().material.color = boundaryCellColor;
+            }
+        }
+
+        cell.isBoundaryCell = (cell.boundaryWalls.Count > 0);
+    }
+
+    private void Update()
+    {
+        if (generationInProgress)
+        {
+            GenerateStep();
+        }
+        else
+        {
+            if (isFinishedRefining == false)
+            {
+                if (!RefineMaze())
+                {
+                    RefineMaze();
+                }
+            }
+        }
+        if (isFinishedRefining && !isFinishedSolvingBlockages)
+        {
+            ReduceBlockages();
+        }
+        if (isFinishedSolvingBlockages && !isFinishedAddingBackWalls)
+        {
+            AddBackWalls();
         }
 
     }
 
-    private IEnumerator GenerateMaze(MazeCell prevCell, MazeCell currCell)
+    private void GenerateStep()
     {
-        if (!currCell.cellWasVisited)
-        {
-            currCell.Visit();
-            //ClearWalls(prevCell, currCell);
+        MazeCell nextCell = GetNextUnvisitedNeighbor(currCell);
 
-            MazeCell nextCell = GetNextUnvisitedNeighbor(currCell);
-            if (nextCell != null) { yield return GenerateMaze(currCell, nextCell); }    
+        if (nextCell != null)
+        {
+            ClearWalls(prevCell, currCell);
+
+            prevCell = currCell;
+            currCell = nextCell;
+            currCell.Visit();
+            unvisitedCells -= 1;
+        }
+        else
+        {
+            foreach (MazeCell cell in mazeLevel)
+            {
+                if (cell.cellWasVisited == true && unvisitedCells == 0)
+                {
+                    innerCells = (mazeWidth - 2) * (mazeDepth - 2);
+                    boundaryCellsCount = boundaryCells.Count;
+                    if (cell.isBoundaryCell == false)
+                    {
+                        boundaryCells.Remove(cell);
+                    }
+                    Debug.Log("All cells have been visited");
+                    generationInProgress = false;
+                    return;
+                }
+            }
+
+            Debug.Log("Current Cell has no unvisted neighbors. Finding remaining unvisited cells...");
+            for (int i = 0; i < mazeWidth; i++)
+            {
+                for (int j = 0; j < mazeDepth; j++)
+                {
+                    for (int k = 0; k < mazeHeight; k++)
+                    {
+                        if ((mazeLevel[i, k, j].cellWasVisited == false))
+                        {
+                            mazeLevel[i, k, j].Visit();
+                            unvisitedCells -= 1;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    public bool RefineMaze()
+    {
+        List<MazeCell> innerCells = new List<MazeCell>();
+
+        foreach (MazeCell cell in mazeLevel)
+        {
+            cell.DeleteRandomRemainingWall(mazeHeight);
         }
 
+
+
+        int wallCount = 0;
+        foreach (MazeCell cell in mazeLevel)
+        {
+            cell.RemoveBoundaryWallsFromRemainingWalls();
+            foreach (GameObject wall in cell.remainingWalls)
+            {
+                wallCount += cell.numOfWalls;
+            }
+        }
+
+        if (wallCount <= (int)((float)totalCells) / minWallPercent)
+        {
+            isFinishedRefining = true;
+            Debug.Log("Maze has been refined");
+        }
+
+        return isFinishedRefining;
+    }
+
+    public void ReduceBlockages()
+    {
+        isFinishedSolvingBlockages = false;
+
+        // get the isolated cells
+        List<MazeCell> isolatedCells = new List<MazeCell>();
+        foreach (MazeCell cell in mazeLevel)
+        {
+            if (cell.remainingWalls.Count == cell.numOfWalls)
+            {
+                isolatedCells.Add(cell);
+                cell.DeleteRandomRemainingWall(mazeHeight);
+            }
+        }
+
+        // calculate the number of isolated cells to remove
+        int numIsolatedCellsToRemove = (int)(maxIsolatedCells * totalCells);
+
+        // select a random isolated cell
+        int randomCellIndex = Random.Range(0, isolatedCells.Count);
+
+        // if the cell is isolated, remove a random wall
+        isolatedCells[randomCellIndex].DeleteRandomRemainingWall(mazeHeight);
+
+
+        isFinishedSolvingBlockages = true;
+    }
+
+    public void AddBackWalls()
+    {
+        isFinishedAddingBackWalls = false;
+
+
+        foreach (MazeCell cell in mazeLevel)
+        {
+            //cell.AddBackWall();
+        }
+
+
+
+
+        isFinishedAddingBackWalls = true;
+    }
+
+    private void StartMazeGeneration()
+    {
+        prevCell = null;
+        currCell = mazeLevel[0, 0, 0]; // Start from the first cell
+        generationInProgress = true;
+        currCell.Visit();
+        unvisitedCells -= 1;
+        prevCell = currCell;
     }
 
     private MazeCell GetNextUnvisitedNeighbor(MazeCell currCell)
     {
         var unvistedCells = GetUnvisitedCells(currCell);
-        return unvistedCells.OrderBy(_ => Random.Range(1, 10)).FirstOrDefault();
+        int max = unvistedCells.Count();
+        return unvistedCells.OrderBy(_ => Random.Range(1, max)).FirstOrDefault();
 
     }
 
@@ -69,101 +347,142 @@ public class MazeGenerator : MonoBehaviour
     {
 
         int x = (int)(currCell.transform.position.x);
+        int y = (int)(currCell.transform.position.y);
         int z = (int)(currCell.transform.position.z);
 
         if ((x + 1) < mazeWidth)
         {
-            var cellToRight = mazeLevel[x + 1, z];
+            var cellToRight = mazeLevel[x + 1, y, z];
             if (cellToRight.cellWasVisited == false)
             {
                 yield return cellToRight;
             }
-
         }
-
-
-
         if ((x - 1) >= 0)
         {
-            var cellToLeft = mazeLevel[x - 1, z];
+            var cellToLeft = mazeLevel[x - 1, y, z];
             if (cellToLeft.cellWasVisited == false)
             {
                 yield return cellToLeft;
             }
-
         }
 
 
         if ((z + 1) < mazeDepth)
         {
-            var cellToFront = mazeLevel[x, z + 1];
+            var cellToFront = mazeLevel[x, y, z + 1];
             if (cellToFront.cellWasVisited == false)
             {
                 yield return cellToFront;
             }
-
         }
-
         if ((z - 1) >= 0)
         {
-            var cellToBack = mazeLevel[x, z - 1];
+            var cellToBack = mazeLevel[x, y, z - 1];
             if (cellToBack.cellWasVisited == false)
             {
                 yield return cellToBack;
             }
+        }
 
+        if (mazeHeight > 1)
+        {
+            if (y + 1 < mazeHeight)
+            {
+                var cellToUp = mazeLevel[x, y + 1, z];
+                if (cellToUp.cellWasVisited == false)
+                {
+                    yield return cellToUp;
+                }
+            }
+            if (y - 1 >= 0)
+            {
+                var cellToDown = mazeLevel[x, y - 1, z];
+                if (cellToDown.cellWasVisited == false)
+                {
+                    yield return cellToDown;
+                }
+            }
         }
 
         int randWidth = Random.Range(0, mazeWidth);
+        int randHeight = Random.Range(0, mazeHeight);
         int randDepth = Random.Range(0, mazeDepth);
 
-        if (mazeLevel[randWidth, randDepth].cellWasVisited == false)
+        if (mazeLevel[randWidth, randHeight, randDepth].cellWasVisited == false)
         {
-            var cellToVisit = mazeLevel[randWidth, randDepth];
+            var cellToVisit = mazeLevel[randWidth, randHeight, randDepth];
             yield return cellToVisit;
         }
-            
-
-
-            
 
     }
-
-
-
-
 
     private void ClearWalls(MazeCell prevCell, MazeCell currCell)
     {
-      
-        
-        if (prevCell.transform.position.x < currCell.transform.position.x) { prevCell.DestroyRightWall(); currCell.DestroyLeftWall(); return; }
-        if (prevCell.transform.position.x > currCell.transform.position.x) { prevCell.DestroyLeftWall(); currCell.DestroyRightWall(); return; }
+        if (prevCell == null) { return; }
 
-        if (prevCell.transform.position.z < currCell.transform.position.z) { prevCell.DestroyFrontWall(); currCell.DestroyBackWall(); return; }
-        if (prevCell.transform.position.z > currCell.transform.position.z) { prevCell.DestroyBackWall(); currCell.DestroyFrontWall(); return; }
+        int prevX = (int)(prevCell.transform.position.x);
+        int prevY = (int)(prevCell.transform.position.y);
+        int prevZ = (int)(prevCell.transform.position.z);
+        int currX = (int)(currCell.transform.position.x);
+        int currY = (int)(currCell.transform.position.y);
+        int currZ = (int)(currCell.transform.position.z);
 
-        if (prevCell.transform.position.y < currCell.transform.position.y) { prevCell.DestroyCieling(); currCell.DestroyFloor(); return; }
-        if (prevCell.transform.position.y > currCell.transform.position.y) { prevCell.DestroyFloor(); currCell.DestroyCieling(); }
+        // Check the difference in positions and remove walls accordingly
+        if (prevX < currX && prevCell.hasRightWall && !prevCell.boundaryWalls.Contains(prevCell.RightWall))
+        {
+            prevCell.DestroyRightWall();
 
+            if (!currCell.boundaryWalls.Contains(currCell.LeftWall))
+            {
+                currCell.DestroyLeftWall();
+            }
+        }
+        else if (prevX > currX && prevCell.hasLeftWall && !prevCell.boundaryWalls.Contains(prevCell.LeftWall))
+        {
+            prevCell.DestroyLeftWall();
+
+            if (!currCell.boundaryWalls.Contains(currCell.RightWall))
+            {
+                currCell.DestroyRightWall();
+            }
+        }
+        else if (prevZ < currZ && prevCell.hasFrontWall && !prevCell.boundaryWalls.Contains(prevCell.FrontWall))
+        {
+            prevCell.DestroyFrontWall();
+
+            if (!currCell.boundaryWalls.Contains(currCell.BackWall))
+            {
+                currCell.DestroyBackWall();
+            }
+        }
+        else if (prevZ > currZ && prevCell.hasBackWall && !prevCell.boundaryWalls.Contains(prevCell.BackWall))
+        {
+            prevCell.DestroyBackWall();
+
+            if (!currCell.boundaryWalls.Contains(currCell.FrontWall))
+            {
+                currCell.DestroyFrontWall();
+            }
+        }
+        else if (prevY < currY && prevCell.hasCeiling && !prevCell.boundaryWalls.Contains(prevCell.Ceiling))
+        {
+            prevCell.DestroyCeiling();
+
+            if (!currCell.boundaryWalls.Contains(currCell.Floor))
+            {
+                currCell.DestroyFloor();
+            }
+        }
+        else if (prevY > currY && prevCell.hasFloor && !prevCell.boundaryWalls.Contains(prevCell.Floor))
+        {
+            prevCell.DestroyFloor();
+
+            if (!currCell.boundaryWalls.Contains(currCell.Ceiling))
+            {
+                currCell.DestroyCeiling();
+            }
+        }
     }
 
-
-
-
-    void Update()
-    {
-            int randWidth = Random.Range(0, mazeWidth);
-            int randDepth = Random.Range(0, mazeDepth);
-            int randHeight = Random.Range(0, mazeHeight);
-            ClearWalls(null, levels[randHeight][randWidth, randDepth]);
-        //for (int i = 0; i < levels.Count; i++)
-        //{
-        //    for (int j = 0;  j < levels[i].Length; j++) 
-        //    {
-
-
-        //    }
-        //}
-    }
 }
